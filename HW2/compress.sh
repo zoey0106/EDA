d57106d8dd92571e3883867cf262f1fe6741c219
zoey0106 <zoezoezoe0106@gmail.com>
2025-04-01 21:38:08 +0800

    4/1 Hw2 compelete

---
+++ HW2/compress.sh
@@ -0,0 +1,10 @@
+cd HW2
+cd src
+make clean
+make
+cd ..
+cd ..
+tar -zvcf CS6135_HW2_113062640.tar.gz HW2
+mv CS6135_HW2_113062640.tar.gz HW2_grading/student/113062640
+cd HW2_grading
+bash HW2_grading.sh
